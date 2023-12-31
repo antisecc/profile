586ba9e4104f9d71091008740c2c69b7ad672a63
Antisec <92209707+antisecc@users.noreply.github.com>
2024-01-01 02:01:39 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,4 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwind from "@astrojs/tailwind";
 import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
